055927ba713f74c7de9fe36c9e018d7e024f22b3
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-07-15 00:17:02 -0600

    finished comments model/routes/controllers

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,6 +4,12 @@ const pizzaController = {
   // get all pizzas
   getAllPizza(req, res) {
     Pizza.find({})
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
       .then((dbPizzaData) => res.json(dbPizzaData))
       .catch((err) => {
         console.log(err);
@@ -12,6 +18,11 @@ const pizzaController = {
   },
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      .select("-__v")
       .then((dbPizzaData) => {
         if (!dbPizzaData) {
           res.status(404).json({ message: "No pizza found with this id" });
@@ -48,14 +59,14 @@ const pizzaController = {
   //delete pizza
   deletePizza({ params }, res) {
     Pizza.findOneAndDelete({ _id: params.id })
-      .then(dbPizzaData => {
+      .then((dbPizzaData) => {
         if (!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found with this id' });
+          res.status(404).json({ message: "No pizza found with this id" });
           return;
         }
         res.json(dbPizzaData);
       })
-      .catch(err => res.status(400).json(err));
+      .catch((err) => res.status(400).json(err));
   },
 };
 
